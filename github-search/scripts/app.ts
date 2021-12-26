3b560999dafaa56725b5cfb07832f735c9f8ed5e
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2021-12-26 17:50:52 +0200

    done project

--- github-search/scripts/app.ts
+++ github-search/scripts/app.ts
@@ -1,1 +1,24 @@
 //TypeScript File
+const user: HTMLElement = document.getElementById('user');
+const apiKey: string = "https://api.github.com/users/";
+const submit: HTMLElement = document.getElementById('submit');
+async function fetchData() {
+    let data: Response = await fetch(`${apiKey}${(<HTMLInputElement>user).value}`);
+    let response = await data.json();
+    document.querySelector(".hero-wrapper").innerHTML = `
+            <div class="hero-image">
+                <img src="${response.avatar_url}" alt="image">
+            </div>
+            <div class="hero__login">#${response.login}</div>
+            <div class="hero__name">${response.name}</div>
+            <div class="hero-testimonial">
+                <div class="repo">Repos: ${response.public_repos}</div>
+                <div class="gist">Gists: ${response.public_gists}</div>
+                <div class="followers">Followers: ${response.followers}</div>
+                <div class="following">Following: ${response.following}</div>
+            </div>
+`;
+}
+
+submit.addEventListener("click", (event: Event) => fetchData())
+
