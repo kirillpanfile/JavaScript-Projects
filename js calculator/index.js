2db65258e8187446defc0ff3e773167ed5fc007d
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-02-08 10:45:50 +0200

    end of js Claculator

---
+++ js calculator/index.js
@@ -0,0 +1,20 @@
+const display = document.getElementById("display")
+const equal = document.getElementById("equal")
+const buttons = Array.from(document.querySelectorAll(".button"))
+
+//find remove last 2 chars of a string js.
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.innerText != "C" && button.innerText != "←" && button.innerText != "=") display.innerText += button.innerText
+        if (button.innerText == "C") display.innerText = ''
+        if (button.innerText == "←") if (display.innerText) display.innerText = `${display.innerText.slice(0, -2)}`
+        if (button.innerText == "=") {
+            try {
+                display.innerText = eval(display.innerText)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    })
+})
